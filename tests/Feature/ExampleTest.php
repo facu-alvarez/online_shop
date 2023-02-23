bb0233d37ebcdeaa5f32bdc0c47ede522f3199cd
Facundo Alvarez <facundo.alvarez@payco.co>
2023-02-23 16:11:34 -0300

    php version

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,21 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
+use JustSteveKing\StatusCode\Http;
+use function Pest\Laravel\get;
 
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_the_application_returns_a_successful_response()
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
-    }
-}
+it('asdasdas', function () {
+    get(
+        route('home'),
+    )->assertStatus(
+        Http::OK()
+    );
+});
